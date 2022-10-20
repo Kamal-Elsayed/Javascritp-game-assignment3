1fda3e19344d2771feb575ba77e78ecad6e2ae00
Kamal-Elsayed <115374745+Kamal-Elsayed@users.noreply.github.com>
2022-10-20 09:15:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,132 +1,44 @@
-// app.js
-
-// Complete logic of game inside this function
-const game = () => {
-	let playerScore = 0;
-	let computerScore = 0;
-	let moves = 0;
-
-
-	// Function to
-	const playGame = () => {
-		const rockBtn = document.querySelector('.rock');
-		const paperBtn = document.querySelector('.paper');
-		const scissorBtn = document.querySelector('.scissor');
-		const playerOptions = [rockBtn,paperBtn,scissorBtn];
-		const computerOptions = ['rock','paper','scissors']
-		
-		// Function to start playing game
-		playerOptions.forEach(option => {
-			option.addEventListener('click',function(){
-
-				const movesLeft = document.querySelector('.movesleft');
-				moves++;
-				movesLeft.innerText = `Moves Left: ${5-moves}`;
-				
-
-				const choiceNumber = Math.floor(Math.random()*3);
-				const computerChoice = computerOptions[choiceNumber];
-
-				// Function to check who wins
-				winner(this.innerText,computerChoice)
-				
-				// Calling gameOver function after 5 moves
-				if(moves == 5){
-					gameOver(playerOptions,movesLeft);
-				}
-			})
-		})
-		
-	}
-
-	// Function to decide winner
-	const winner = (player,computer) => {
-		const result = document.querySelector('.result');
-		const playerScoreBoard = document.querySelector('.p-count');
-		const computerScoreBoard = document.querySelector('.c-count');
-		player = player.toLowerCase();
-		computer = computer.toLowerCase();
-		if(player === computer){
-			result.textContent = 'Tie'
-		}
-		else if(player == 'rock'){
-			if(computer == 'paper'){
-				result.textContent = 'Computer Won';
-				computerScore++;
-				computerScoreBoard.textContent = computerScore;
-
-			}else{
-				result.textContent = 'Player Won'
-				playerScore++;
-				playerScoreBoard.textContent = playerScore;
-			}
-		}
-		else if(player == 'scissors'){
-			if(computer == 'rock'){
-				result.textContent = 'Computer Won';
-				computerScore++;
-				computerScoreBoard.textContent = computerScore;
-			}else{
-				result.textContent = 'Player Won';
-				playerScore++;
-				playerScoreBoard.textContent = playerScore;
-			}
-		}
-		else if(player == 'paper'){
-			if(computer == 'scissors'){
-				result.textContent = 'Computer Won';
-				computerScore++;
-				computerScoreBoard.textContent = computerScore;
-			}else{
-				result.textContent = 'Player Won';
-				playerScore++;
-				playerScoreBoard.textContent = playerScore;
-			}
-		}
-	}
-
-	// Function to run when game is over
-	const gameOver = (playerOptions,movesLeft) => {
-
-		const chooseMove = document.querySelector('.move');
-		const result = document.querySelector('.result');
-		const reloadBtn = document.querySelector('.reload');
-
-		playerOptions.forEach(option => {
-			option.style.display = 'none';
-		})
-
-	
-		chooseMove.innerText = 'Game Over!!'
-		movesLeft.style.display = 'none';
-
-		if(playerScore > computerScore){
-			result.style.fontSize = '2rem';
-			result.innerText = 'You Won The Game'
-			result.style.color = '#308D46';
-		}
-		else if(playerScore < computerScore){
-			result.style.fontSize = '2rem';
-			result.innerText = 'You Lost The Game';
-			result.style.color = 'red';
-		}
-		else{
-			result.style.fontSize = '2rem';
-			result.innerText = 'Tie';
-			result.style.color = 'grey'
-		}
-		reloadBtn.innerText = 'Restart';
-		reloadBtn.style.display = 'flex'
-		reloadBtn.addEventListener('click',() => {
-			window.location.reload();
-		})
-	}
-
-
-	// Calling playGame function inside game
-	playGame();
-	
+function game(Rock, Paper, Scissors) {
+    let UserPlay = 0,
+    ComputerPlay = 0,
+    moves = 0
 }
+function SingleRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection)
+    console.log(Draw);
 
-// Calling the game function
-game();
+    else if (playerSelection == Paper)
+        if (computerSelection == Scissors){
+            console.log("Computer Win");
+        }
+        
+       
+    else if (playerSelection == Paper)
+        if (computerSelection == Rock) {
+            console.log("You Win");
+        }
+
+    else if (playerSelection == Scissors)
+        if (computerSelection == Rock){
+            console.log("Computer Win");
+        }
+    
+    else if (playerSelection == Scissors)
+        if (computerSelection == Paper){
+            console.log("You Win");
+        }
+
+    else if (playerSelection == Rock)
+        if (computerSelection == Paper){
+            console.log("Computer Win");
+        }
+    
+    else if (playerSelection == Rock)
+        if (computerSelection == Scissors){
+            console.log("You Win");
+        }
+
+    
+
+}
+return game
